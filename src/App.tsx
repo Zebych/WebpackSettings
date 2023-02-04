0bfac6d60a8f02e263cdda3cd31aaba763169f9b
Zebych <81613417+Zebych@users.noreply.github.com>
2023-02-04 12:51:10 +0300

    Настройка css modules

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Counter} from './components/Counter';
+import './index.scss'
+
+const App = () => {
+    return (
+        <div className='app'>
+            lkhdsasd
+            <Counter/>
+        </div>
+    );
+};
+
+export default App;
