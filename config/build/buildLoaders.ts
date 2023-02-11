48d0776c74c3c0d38354f29de8968cc897ee5aa3
Zebych <81613417+Zebych@users.noreply.github.com>
2023-02-11 11:44:59 +0300

    Настройка абсолютных импортов. Построение структуры папок. Введение архитектуры.

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,29 +1,29 @@
-import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import {BuildOptions} from './types/config';
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
-            // Creates `style` nodes from JS strings
             isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
             {
-                loader: 'css-loader',
+                loader: "css-loader",
                 options: {
                     modules: {
                         auto: (resPath: string) => Boolean(resPath.includes('.module.')),
                         localIdentName: isDev
-                            ? '[path][name]__[local]'
-                            : '[ hash:base64:8]'
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]'
                     },
                 }
             },
-            // Compiles Sass to CSS
-            'sass-loader',
+            "sass-loader",
         ],
     }
+
+    // Если не используем тайпскрипт - нужен babel-loader
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -32,6 +32,6 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
     return [
         typescriptLoader,
-        cssLoader
+        cssLoader,
     ]
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss'
-import {AboutPageAsync} from './pages/AbiutPage/AboutPage.async';
-import {MainPageAsync} from './pages/MainPage/MainPage.async';
-import {useTheme} from './theme/useTheme';
-import {classNames} from './helpers/classNames/classNames';
+
+import {classNames} from 'shared/lib/classNames/classNames';
+import {useTheme} from 'app/providers/ThemeProviders';
+import {AboutPage} from 'pages/AbiutPage';
+import {MainPage} from 'pages/MainPage';
+
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
@@ -15,8 +17,8 @@ const App = () => {
             <Link to={'/about'}> О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
 
