bd3ef6625391524f28bb7d27d43d8c89797f6b4e
Zebych <81613417+Zebych@users.noreply.github.com>
2023-02-19 16:49:12 +0300

    Sidebar. Layout приложения

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,15 +5,19 @@ import {classNames} from 'shared/lib/classNames/classNames';
 import {useTheme} from 'app/providers/ThemeProviders';
 import {AppRouter} from 'app/providers/router';
 import {Navbar} from 'widgests/Navbar';
+import {Sidebar} from 'widgests/Sidebar';
 
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme()
+    const {theme} = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar/>
-            <AppRouter/>
-            <button onClick={toggleTheme}>TOGGLE</button>
+            <div className={'content-page'}>
+                <Sidebar/>
+                <AppRouter/>
+            </div>
+
         </div>
     );
 };
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -4,7 +4,8 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] =[
     return [
         cls,
         ...additional.filter(Boolean),
-        ...Object.entries(mods).filter(([value]) => Boolean(value))
+        ...Object.entries(mods)
+            .filter(([className, value]) => Boolean(value))
             .map(([className]) => className)
     ]
         .join(' ')
