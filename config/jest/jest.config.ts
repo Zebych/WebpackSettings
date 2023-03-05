f7481ee44ca4fd25470e0fc1c17e8b6d3bfa3eed
Zebych <81613417+Zebych@users.noreply.github.com>
2023-03-05 22:24:08 +0300

    add redux store, add first entity

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -6,16 +6,9 @@
 import path from 'path';
 
 export default {
-    // All imported modules in your tests should be mocked automatically
-    // automock: false,
-
-    // Stop running tests after `n` failures
-    // bail: 0,
-
-    // The directory where Jest should store its cached dependency information
-    // cacheDirectory: "C:\\Users\\tim\\AppData\\Local\\Temp\\jest",
-
-    // Automatically clear mock calls, instances and results before every test
+    globals: {
+        __IS_DEV__: true,
+    },
     clearMocks: true,
     testEnvironment: 'jsdom',
     coveragePathIgnorePatterns: [
@@ -45,6 +38,16 @@ export default {
         '\\.(s?css)$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
     },
+    // All imported modules in your tests should be mocked automatically
+    // automock: false,
+
+    // Stop running tests after `n` failures
+    // bail: 0,
+
+    // The directory where Jest should store its cached dependency information
+    // cacheDirectory: "C:\\Users\\tim\\AppData\\Local\\Temp\\jest",
+
+    // Automatically clear mock calls, instances and results before every test
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
 
