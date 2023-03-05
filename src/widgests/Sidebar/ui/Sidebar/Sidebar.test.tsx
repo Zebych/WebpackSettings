6fe681174848ca63209cdf677ad06acc77153ac4
Alexandr Zabirov <sanechek050187@gmail.com>
2023-03-05 22:32:07 +0300

    add redux store, add first entity (#4)
    * add redux store, add first entity
    
    * fix screenshot testing

--- src/widgests/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgests/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { componentRouter } from 'shared/lib/tests/componentRender/componentRender';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
     test('Проверка отрисовки текста в кнопке', () => {
-        componentRouter(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('Проверка на сворачивание,разворачивание', () => {
-        componentRouter(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
